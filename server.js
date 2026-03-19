020cafcbdb96fd0443ddcc75a7b9b9d48de33eac
vrinii824 <vrindajspv@gmail.com>
2026-03-19 23:46:42 +0530

    Updated website changes

---
+++ server.js
@@ -0,0 +1,37 @@
+﻿const http = require('http');
+const fs = require('fs');
+const path = require('path');
+
+const root = process.cwd();
+const mime = {
+  '.html': 'text/html',
+  '.css': 'text/css',
+  '.js': 'text/javascript',
+  '.json': 'application/json',
+  '.png': 'image/png',
+  '.jpg': 'image/jpeg',
+  '.jpeg': 'image/jpeg',
+  '.svg': 'image/svg+xml',
+  '.ico': 'image/x-icon'
+};
+
+const server = http.createServer((req, res) => {
+  let urlPath = req.url.split('?')[0];
+  if (urlPath === '/' || urlPath === '') urlPath = '/index.html';
+  const filePath = path.join(root, decodeURIComponent(urlPath));
+
+  fs.stat(filePath, (err, stat) => {
+    if (err || !stat.isFile()) {
+      res.statusCode = 404;
+      res.end('Not found');
+      return;
+    }
+    const ext = path.extname(filePath).toLowerCase();
+    res.setHeader('Content-Type', mime[ext] || 'application/octet-stream');
+    fs.createReadStream(filePath).pipe(res);
+  });
+});
+
+server.listen(8000, '127.0.0.1', () => {
+  console.log('Server running on http://localhost:8000');
+});
